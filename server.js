904416228e8b42a2e4f8932d1d1090064e64a076
elgebaly671 <abdelrhman.elgebaly@student.guc.edu.eg>
2026-08-10 19:43:30 +0300

    Stage 1: signup and login routes working

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
-const {createClient} = require('@supabase/supabase-js')
+const { createClient } = require('@supabase/supabase-js')
 
 app.use(cors());
+app.use(express.json());
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
@@ -22,6 +23,59 @@ supabase.auth.getSession().then(({ error }) => {
     }
 })
 
-app.listen(port, ()=>{
+
+app.post("/auth/signup", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password)
+            return res.status(400).json({
+                error: "Missing email or password"
+            })
+        const { data, error } = await supabase.auth.signUp({
+            email,
+            password
+        })
+        if(data){
+            res.sendStatus(201);
+        }else if(error){
+            res.status(400).json({
+                error
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error: error.message
+        })
+    }
+})
+
+app.post("/auth/login", async (req, res)=>{
+    try {
+        const { email, password } = req.body;
+        if(!email || !password)
+            return res.status(400).json({
+                error: "Missing email or password"
+            })
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if(data){
+            res.status(200).json({
+                access_token: data.session.access_token,
+                refresh_token: data.session.refresh_token
+            });
+        }else if(error){
+            res.status(400).json({
+                error
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error: error.message
+        })
+    }
+})
+app.listen(port, () => {
     console.log('Server is up')
 })
