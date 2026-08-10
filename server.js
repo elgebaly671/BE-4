455d0962f55e391b27a6388a3d71a32773b0d0d1
elgebaly671 <abdelrhman.elgebaly@student.guc.edu.eg>
2026-08-10 20:02:24 +0300

    Stage 3: profile route token verification

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ supabase.auth.getSession().then(({ error }) => {
 app.post("/auth/signup", async (req, res) => {
     try {
         const { email, password } = req.body;
-        if(!email || !password)
+        if (!email || !password)
             return res.status(400).json({
                 error: "Missing email or password"
             })
@@ -35,9 +35,36 @@ app.post("/auth/signup", async (req, res) => {
             email,
             password
         })
-        if(data){
+        if (data) {
             res.sendStatus(201);
-        }else if(error){
+        } else if (error) {
+            res.status(400).json({
+                error
+            })
+        }
+    } catch (error) {
+        return res.json({
+            error: error.message
+        })
+    }
+})
+app.post("/auth/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password)
+            return res.status(400).json({
+                error: "Missing email or password"
+            })
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if (data) {
+            res.status(200).json({
+                access_token: data.session.access_token,
+                refresh_token: data.session.refresh_token
+            });
+        } else if (error) {
             res.status(400).json({
                 error
             })
@@ -49,31 +76,24 @@ app.post("/auth/signup", async (req, res) => {
     }
 })
 
-app.post("/auth/login", async (req, res)=>{
+app.get('/public/info', (req, res) => {
+    return res.json({
+        message: "Welcome stranger! This info is public."
+    })
+})
+app.get("/protected/profile", async (req, res) => {
     try {
-        const { email, password } = req.body;
-        if(!email || !password)
-            return res.status(400).json({
-                error: "Missing email or password"
-            })
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password
+        const token = req.headers.authorization?.split(' ')[1];
+        const { data, error } = await supabase.auth.getUser(token);
+        if (error || !data.user) return res.status(401).json({ error: "Access token required" })
+
+        return res.status(200).json({
+            id: data.user.id,
+            email: data.user.email,
+            created_At: data.user.created_at
         })
-        if(data){
-            res.status(200).json({
-                access_token: data.session.access_token,
-                refresh_token: data.session.refresh_token
-            });
-        }else if(error){
-            res.status(400).json({
-                error
-            })
-        }
     } catch (error) {
-        return res.json({
-            error: error.message
-        })
+        res.json({ error: error.message })
     }
 })
 app.listen(port, () => {
